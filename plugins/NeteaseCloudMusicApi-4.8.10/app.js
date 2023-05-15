c4f2543f0ba2a31456316fcb291abd63f616fcc6
zhanghong <11972999127@qq.com>
2023-05-15 15:48:36 +0800

    feat:插件启动优化

--- plugins/NeteaseCloudMusicApi-4.8.10/app.js
+++ plugins/NeteaseCloudMusicApi-4.8.10/app.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
+let server=null;
 async function start() {
   // 如果需要手动修改anonymous_token，需要注释generateConfig调用
-  require('./server').serveNcmApi({
+  server=require('./server').serveNcmApi({
     checkVersion: true,
   })
+}
+async function end() {
+  // 关闭服务
+  server.close();
+  server=null;
 }
 // start()
 module.exports={
   start,
+  end,
+  server,
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const {start,end,server}= require('../plugins/NeteaseCloudMusicApi-4.8.10/app.js');
 
 router.get('/menu',async ()=>{
   
+})
+//手动打开关闭网易云音乐插件
+router.post('/wyyMusic',async (req,res)=>{
+  const {flag}=req.body;
+  if (flag) {
+    if (server) {
+      res.send({
+        code:0,
+        message:'音乐插件服务已经正在运行！'
+      })
+    } else {
+      await start();
+      res.send({
+        code:1,
+        message:'音乐插件服务启动成功！'
+      })
+    }
+  } else {
+    if (server) {
+      await end();
+      res.send({
+        code:1,
+        message:'音乐插件服务关闭成功！'
+      })
+    } else {
+      res.send({
+        code:0,
+        message:'音乐插件服务已经处于关闭状态！'
+      })
+    }
+  }
 })
 
 module.exports = router;
